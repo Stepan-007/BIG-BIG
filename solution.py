07a869f947c093b6b2a2c5db0123cf2ecc804c89
Mikhail <medved.nnov@gmail.com>
2023-02-19 23:45:08 +0300

    BIG - 2.1 (PyQt5 Edition)

--- solution.py
+++ solution.py
@@ -1,26 +1,63 @@
 import os
-import pygame
+import sys
+
 import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
 
-map_server = "http://static-maps.yandex.ru/1.x/"
-map_params = {'ll': '135.125529,-27.306904',
-              'spn': '40.9,25.9',
-              'l': 'sat'}
-response = requests.get(map_server, map_params)
+SCREEN_SIZE = [600, 450]
 
-if not response:
-    pass
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
 
-os.remove(map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+
+        self.z = 17
+
+        self.get_image()
+
+    def get_image(self):
+        map_server = "http://static-maps.yandex.ru/1.x/"
+        map_params = {'ll': '37.620070,55.753630',
+                      'z': self.z,
+                      'l': 'map'}
+        response = requests.get(map_server, map_params)
+
+        # Запишем полученное изображение в файл.
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+        pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            if self.z < 17:
+                self.z += 1
+            self.get_image()
+        elif event.key() == Qt.Key_PageDown:
+            if self.z > 0:
+                self.z -= 1
+            self.get_image()
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
